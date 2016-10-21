9582460aea988e6fd8c9bec8dfaed050067b896c
Jose Antonio Dorado <jadorado@emergya.com>
2016-10-21 10:40:18 +0200

    Script modified

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -75,6 +75,7 @@ php bin/console doctrine:database:create
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load -n
 php bin/console cache:clear
+php bin/console cache:clear --env=prod
 sudo chmod -R 777 $SYMFONYPATH/var/cache/*
 sudo chmod -R 777 $SYMFONYPATH/var/logs/*
 
